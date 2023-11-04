738f3d3be699a7aaf4688fed797213083c4ee4d9
3p1c0w3nd <3p1c0w3nd@1337.com>
2023-11-04 18:22:54 -0500

    se arreglo el bug de los enlaces

--- src/components/header/ComponentMainSite.jsx
+++ src/components/header/ComponentMainSite.jsx
@@ -16,8 +16,8 @@ const ComponentMainSite = () =>{
             <div>{renderMenu}</div>
            <div className="flex gap-4 mt-9">
                <p><img src="./twitter.png" width={20} height={20} onClick={() => funciones.gotoLocation("https://twitter.com/3p1c0w3nd")} style={{cursor:"pointer"}}/></p>
-               <p><img src="./linkenin.png" width={20} height={20} onClick={() => funciones.gotoLocation("https://github.com/3p1c0s3nd")} style={{cursor:"pointer"}}/></p>
-               <p><img src="./github.png" width={20} height={20} onClick={() => funciones.gotoLocation("https://www.linkedin.com/in/edwin-fajardo-murillo-88a288287/")} style={{cursor:"pointer"}}/></p>
+               <p><img src="./linkenin.png" width={20} height={20} onClick={() => funciones.gotoLocation("https://www.linkedin.com/in/edwin-fajardo-murillo-88a288287/")} style={{cursor:"pointer"}}/></p>
+               <p><img src="./github.png" width={20} height={20} onClick={() => funciones.gotoLocation("https://github.com/3p1c0s3nd")} style={{cursor:"pointer"}}/></p>
            </div>
         </div>
     )
